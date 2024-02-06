8dc82f917c38e843a7aaadfc230e9ab0e867797f
Waleed Ahmed <w29ahmed@uwaterloo.ca>
2024-02-05 22:44:03 -0800

    checkpoint: got divs setup, added image, text, and buttons

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
   import { onMount } from "svelte";
   import ThemeToggleSwitch from "./components/ThemeToggleSwitch.svelte";
+  import walle_eve_image from "./assets/walle_eve_no_bg_heart.png";
+  // import walle_eve_image from "./assets/walle_eve.jpg";
+
   import "./app.css";
 
   // Directly initialize isDarkTheme based on localStorage or default to light theme
@@ -19,6 +22,43 @@
   });
 </script>
 
-<body class="bg-red-300 dark:bg-gray-900 min-h-screen">
+<style>
+  p, button {
+    font-family: 'Pangolin', cursive;
+  }
+</style>
+
+<body class="bg-red-300 dark:bg-gray-900">
   <ThemeToggleSwitch bind:isDarkTheme />
+
+  <div class="flex flex-col items-center justify-center min-h-screen px-4">
+    <!-- Wrapper div to align content to the width of the image -->
+    <!-- inline-block will shrink to fit the largest item -->
+    <div class="inline-block">
+
+      <!-- Top Row: Image -->
+      <div class="w-auto"> <!-- width auto for the container to fit the content -->
+        <!-- max height to ensure image is not too big -->
+        <img src={walle_eve_image} alt="Valentine's Image" class="w-full">
+      </div>
+
+      <!-- Middle Row: Text -->
+      <!-- Full width of the parent -->
+      <div class="w-full text-center">
+        <p class="text-4xl text-gray-900 dark:text-gray-200 my-4">Will you be my Valentine?</p>
+      </div>
+
+      <!-- Bottom Row: Buttons -->
+      <!-- Full width of the parents -->
+      <div class="grid grid-cols-2 w-full p">
+        <div class="flex items-center justify-center">
+          <button class="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-700 transition">Yes</button>
+        </div>
+        <div class="flex items-center justify-center">
+          <button class="px-4 py-2 bg-gray-500 text-white rounded hover:bg-gray-700 transition">No</button>
+        </div>
+      </div>
+
+    </div>
+  </div>
 </body>
